2f7534c43c24dc4edcc076aa145f8c7d96231054
victormak <victormak357@gmail.com>
2016-03-09 12:59:35 +0800

    add change_time.sh

--- change_time.sh
+++ change_time.sh
@@ -2,7 +2,7 @@
 
 str=$(./sx-3000-ip-generation.tcl)
 echo $str
-rack_num=`echo "$str" | grep rack_num | sed 's/^rack_num=//'`
+rack_num=`echo "$str" | grep rack_num |sed 's/^rack_num=//g'`
 imb_num=`echo "$str" | grep imb_num | sed 's/^imb_num=//'`
 
 #rack=$(echo $str | cut -d ":" -f 1)
