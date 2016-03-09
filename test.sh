350970046b2ebf7812240ac50e0a975ca184ef1f
victormak <victormak357@gmail.com>
2016-03-09 12:59:35 +0800

    change sth

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 serialNum=$1
 
-#TODO: serial number verification
-
-
-serialNumLength=${#serialNum}
-lastTwoDigts=${serialNum:(serialNumLength-2):(serialNumLength-1)}
+serialNumLenght=${#serialNum}
+lastTwoDigts=${serialNum:(serialNumLength-2):(serialNumLenght-1)}
+if [ ${lastTwoDigts} -eq 00 ]; then
+	lastTwoDigts=100
+fi
 ipPrefix='192.168.1.'
 targetSOMIp=$ipPrefix${lastTwoDigts}
 targetPPCIp=$ipPrefix$(($lastTwoDigts+100))
